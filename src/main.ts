13b9e63a72da6cd4b3a025ceaf1849e7e632cba1
eduardonunespp <carlospn200406@gmail.com>
2024-01-05 20:11:50 -0300

    feat: add

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.APP_PORT, '0.0.0.0');
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 
 bootstrap();
