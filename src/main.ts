a5450995868b6e3233c333a4636f5252138e693c
eduardonunespp <carlospn200406@gmail.com>
2024-01-06 13:03:54 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cors());
 
   const config = new DocumentBuilder()
     .setTitle('Todo Api Documentation')
@@ -27,7 +30,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(process.env.PORT || 80, '0.0.0.0');
 }
 
 bootstrap();
